e91860c201a78f39a0be06f68015ca45ae5c7154
varleyn <varleyn@tcd.ie>
2018-09-29 18:04:42 +0100

    Added TODO note

--- LCA1/src/BinaryTree.java
+++ LCA1/src/BinaryTree.java
@@ -1,7 +1,13 @@
 
 public class BinaryTree {
 	
-	
+	//TODO: This algorithm will only work assuming both the nodes
+	//      n1 and n2 are contained in the binary tree. If, for e.g,
+	//      node n1 is not in the tree beginning at root but node
+	//      n2 is, this algorithm will return the lowest ancestor (i.e parent) 
+	//      of n2. As nodes n1 and n2 clearly have no common ancestors in this 
+	//      situation, null should be returned.
+	//         Fix this!!!
 	public static BTNode lca(BTNode root, BTNode n1, BTNode n2){
 		
 		if(root == null) return null;
